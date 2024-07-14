22313785107f478eb922f9b32200b6b0807e7030
junaidhkn <junaidhkniazi@gmail.com>
2024-07-14 21:29:51 +0500

    server actions

---
+++ actions/auth.ts
@@ -0,0 +1,7 @@
+'use server'
+
+import { cookies } from 'next/headers'
+import { signin, signup } from '@/utils/authTools'
+import { z } from 'zod'
+import { redirect } from 'next/navigation'
+import { COOKIE_NAME } from '@/utils/constants'
